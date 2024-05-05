25b3083427ef965e2a95f7c5fe5d0aafd4894c9b
Axel <axel.vivenot@outlook.fr>
2024-05-05 11:25:40 +0200

    added the node files from tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/view/html/**/*.html"],
+  theme: {
+    fontFamily: {
+      'header': ['Poppins', 'Roboto', 'Open Sans'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
