c30718cb52e8aa80ab0bf36dbc2ba1d34a8c6b49
Hy3z <axel.vivenot@outlook.fr>
2024-07-11 20:03:50 +0200

    again

--- src/auth/auth.go
+++ src/auth/auth.go
@@ -219,7 +219,8 @@ func hasRoles(c echo.Context, access_token string, req_roles []string) bool {
 
 func HasTokenMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		c.Request().Header.Set("Access-Control-Allow-Origin", "*")
+		c.Response().Header().Add("Access-Control-Allow-Origin", "*")
+		c.Request().Header.Add("Access-Control-Allow-Origin", "*")
 		tokenPresent, jwt := hasToken(c)
 		if tokenPresent {
 			if jwt != nil {
